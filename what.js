cfbdf2f58edfa4e7a15eb649fbedecd7bebf3164
liheng <1061555401@qq.com>
2020-10-22 21:56:10 +0800

    更改任务列表

--- what.js
+++ what.js
@@ -1,17 +1,18 @@
-var findDisappearedNumbers = function(nums) {
-  if(!nums.length) return []
-  nums.sort((a,b) => a-b);
-  let arr = []
-  if(nums[0] !== 1) arr.push(1);
-   
-  for(let i = 0; i < nums.length - 1;i++) {
-    if(nums[i + 1] - nums[i] > 1 ) {
-      for(let j = nums[i] + 1;j < nums[i + 1]; j++) {
-        arr.push(j);
-      }
+var findDisappearedNumbers = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[Math.abs(nums[i]) - 1] > 0) {
+      nums[Math.abs(nums[i]) - 1] *= -1
     }
   }
 
-  return arr;
+  console.log(nums);
+  let arr = []
+  for (let j = 0; j < nums.length; j++) {
+    if(nums[j] > 0) {
+      arr.push(j + 1);
+    }
+  }
+  return arr
 };
-console.log(findDisappearedNumbers([2,3,4,5,10]));
+
+console.log(findDisappearedNumbers([4,3,2,7,8,2,3,1]));
